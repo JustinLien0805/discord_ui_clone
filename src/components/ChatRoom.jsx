838cd94291f4a90674aa091319abf3636e0bd7ce
JustinLien0805 <6998dragoan@gmail.com>
2022-06-25 17:58:27 +0800

    change background and scrollbar

--- src/components/ChatRoom.jsx
+++ src/components/ChatRoom.jsx
@@ -4,8 +4,8 @@ const ChatRoom = () => {
   return (
     <div className="flex-grow flex flex-col">
       {/* chat container */}
-      <div className="bg-slate-600 h-screen  overflow-y-scroll">
-        <div className="flex flex-col justify-start items-center">
+      <div className="bg-black h-screen  overflow-y-scroll scrollbar-thumb-neutral-500 scrollbar-track-black scrollbar-thin ">
+        <div className="flex flex-col justify-start items-center ">
           <input
             type="text"
             className="fixed bottom-5 h-10 w-1/2 rounded-lg shadow-lg text-gray-800 text-lg z-50 bg-slate-400"
@@ -14,45 +14,52 @@ const ChatRoom = () => {
         <div className="mb-20 flex flex-col-reverse justify-start mx-16 xl:mx-32 ">
           <Comment
             name="Guava"
-            content="YT上傳要很久欸跟綠島一樣久真假那個佔18趴欸對啊是啊那一個作業佔18趴那是心得不是感想"
+            content="Lorem ipsum dolor sit amet, consectetur adipiscing elit."
           />
           <Comment
             name="Guava"
-            content="YT上傳要很久欸跟綠島一樣久真假那個佔18趴欸對啊是啊那一個作業佔18趴那是心得不是感想"
+            content="loremipsum dolor sit amet, consectetur adipiscing elit."
           />
           <Comment
             name="Guava"
-            content="YT上傳要很久欸跟綠島一樣久真假那個佔18趴欸對啊是啊那一個作業佔18趴那是心得不是感想"
+            content="loremipsum dolor sit amet, consectetur adipis"
           />
           <Comment
             name="Guava"
-            content="YT上傳要很久欸跟綠島一樣久真假那個佔18趴欸對啊是啊那一個作業佔18趴那是心得不是感想"
+            content="loremipsum dolor sit amet, consectetur adipis"
           />
           <Comment
             name="Guava"
-            content="YT上傳要很久欸跟綠島一樣久真假那個佔18趴欸對啊是啊那一個作業佔18趴那是心得不是感想"
+            content="loremipsum dolor sit amet, consectetur adipis"
           />
           <Comment
             name="Guava"
-            content="YT上傳要很久欸跟綠島一樣久真假那個佔18趴欸對啊是啊那一個作業佔18趴那是心得不是感想"
+            content="loremipsum dolor sit amet, consectetur adipis"
           />
           <Comment
             name="Guava"
-            content="YT上傳要很久欸跟綠島一樣久真假那個佔18趴欸對啊是啊那一個作業佔18趴那是心得不是感想"
+            content="Lorem ipsum dolor sit amet, consectetur adipiscing elit."
           />
           <Comment
             name="Guava"
-            content="YT上傳要很久欸跟綠島一樣久真假那個佔18趴欸對啊是啊那一個作業佔18趴那是心得不是感想"
+            content="loremipsum dolor sit amet, consectetur adipiscing elit."
           />
           <Comment
             name="Guava"
-            content="YT上傳要很久欸跟綠島一樣久真假那個佔18趴欸對啊是啊那一個作業佔18趴那是心得不是感想"
+            content="lorem Ipsum dolor sit amet, consectetur adipiscing elit."
           />
           <Comment
             name="Guava"
-            content="YT上傳要很久欸跟綠島一樣久真假那個佔18趴欸對啊是啊那一個作業佔18趴那是心得不是感想"
+            content="loremipsum dolor sit amet, consectetur adipis"
           />
-         
+          <Comment
+            name="Guava"
+            content="lorem Ipsum dolor sit amet, consectetur adipiscing elit."
+          />
+          <Comment
+            name="Guava"
+            content="loremipsum dolor sit amet, consectetur adipis"
+          />
         </div>
       </div>
     </div>
